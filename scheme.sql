043f8447fa3504166e06eb80f0d7b151a7d73c08
YeongCheon <kyc1682@gmail.com>
2016-03-10 16:05:38 +0900

    scheme change

--- scheme.sql
+++ scheme.sql
@@ -1,29 +1,42 @@
-DROP TABLE IF EXISTS `stockbot_kospi`;
-CREATE TABLE IF NOT EXISTS `stockbot_kospi`(
-       `kospi_code` CHAR(6) PRIMARY KEY,
-       `kospi_name` VARCHAR(20) NOT NULL,
-       `kospi_category_code` CHAR(6) NOT NULL,
-       `kospi_category` VARCHAR(50) NOT NULL,
-       `kospi_stock_total` BIGINT UNSIGNED DEFAULT 0,
-       `kospi_capital` NUMERIC(15,2),
-       `kospi_facevalue` NUMERIC(15,2),
-       `kospi_currency` CHAR(10),
-       `kospi_tel` CHAR(20),
-       `kospi_address` VARCHAR(50),
-       `kospi_totalcount` INT UNSIGNED
+DROP TABLE IF EXISTS `stock`;
+CREATE TABLE IF NOT EXISTS `stock`(
+       `code` CHAR(6),
+       `market` CHAR(6),
+       `name` VARCHAR(20) NOT NULL,
+       `category_code` CHAR(6) NOT NULL,
+       `category` VARCHAR(50) NOT NULL,
+       `total` BIGINT UNSIGNED DEFAULT 0,
+       `capital` NUMERIC(15,2),
+       `facevalue` NUMERIC(15,2),
+       `currency` CHAR(10),
+       `tel` CHAR(20),
+       `address` VARCHAR(50),
+       `totalcount` INT UNSIGNED,
+       PRIMARY KEY(code, market)
 );
 
-DROP TABLE IF EXISTS `stockbo_board`;
-CREATE TABLE IF NOT EXISTS `stockbot_board`(
-       `board_id` CHAR(9) PRIMARY KEY, /*for yahoo finance.(ex : 005930.KS)*/
-       `board_name` VARCHAR(20)  DEFAULT NULL
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user`(
+       `email` VARCHAR(30) PRIMARY KEY,
+       `name` VARCHAR(30) NOT NULL
 );
 
-DROP TABLE IF EXISTS `stockbot_kospi_history`;
-CREATE TABLE IF NOT EXISTS `stockbot_kospi_history`(
-       `history_id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-       `history_code` CHAR(9) REFERENCES stockbot_kospi(kospi_code),
-       `history_ask` NUMERIC(15,2) NOT NULL,
-       `history_bid` NUMERIC(15,2) NOT NULL,
-       `history_datetime` TIMESTAMP
+DROP TABLE IF EXISTS `stock_log`;
+CREATE TABLE IF NOT EXISTS `stock_log`(
+       `id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+       `stock_code` CHAR(6) REFERENCES stock(code),
+       `stock_market` CHAR(6) REFERENCES stock(market),
+       `ask` NUMERIC(15,2) NOT NULL,
+       `bid` NUMERIC(15,2) NOT NULL,
+       `datetime` TIMESTAMP
+);
+
+DROP TABLE IF EXISTS `trade_log`;
+CREATE TABLE IF NOT EXISTS `trade_log`(
+       `id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+       `user_email` VARCHAR(30) REFERENCES user(`email`),
+       `stock_code` CHAR(6) REFERENCES stock(code),
+       `stock_market` CHAR(6) REFERENCES stock(market),
+       `trade_type` CHAR(3) CHECK (`trade_type` IN('ask','bid')),
+       `price` NUMERIC(15,2) NOT NULL
 );
