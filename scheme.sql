527d99b9fb6e694ed2d6087a59754ebb065eb59f
YeongCheon <kyc1682@gmail.com>
2016-05-05 20:48:20 +0900

    update scheme

--- scheme.sql
+++ scheme.sql
@@ -1,3 +1,4 @@
+/*주식 종목정보*/
 DROP TABLE IF EXISTS `stock`;
 CREATE TABLE IF NOT EXISTS `stock`(
        `code` CHAR(6),
@@ -15,28 +16,42 @@ CREATE TABLE IF NOT EXISTS `stock`(
        PRIMARY KEY(code, market)
 );
 
+/*사용자 정보*/
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user`(
        `email` VARCHAR(30) PRIMARY KEY,
        `name` VARCHAR(30) NOT NULL
 );
+INSERT INTO user(`email`, `name`) VALUES(`kyc1682@gmail.com`, `YeongCheon`)
 
+/*주식 등락기록*/
 DROP TABLE IF EXISTS `stock_log`;
 CREATE TABLE IF NOT EXISTS `stock_log`(
        `id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-       `stock_code` CHAR(6) REFERENCES stock(code),
-       `stock_market` CHAR(6) REFERENCES stock(market),
-       `ask` NUMERIC(15,2) NOT NULL,
-       `bid` NUMERIC(15,2) NOT NULL,
-       `datetime` TIMESTAMP
+       `stock_code` CHAR(6) REFERENCES stock(code) NOT NULL,
+       `stock_market` CHAR(6) REFERENCES stock(market) NOT NULL,
+       `ask` NUMERIC(15,2) NOT NULL, /*매도가*/
+       `bid` NUMERIC(15,2) NOT NULL, /*매수가*/
+       `datetime` TIMESTAMP NOT NULL
 );
 
+/*주식 거래기록*/
 DROP TABLE IF EXISTS `trade_log`;
 CREATE TABLE IF NOT EXISTS `trade_log`(
        `id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-       `user_email` VARCHAR(30) REFERENCES user(`email`),
-       `stock_code` CHAR(6) REFERENCES stock(code),
-       `stock_market` CHAR(6) REFERENCES stock(market),
-       `trade_type` CHAR(3) CHECK (`trade_type` IN('ask','bid')),
-       `price` NUMERIC(15,2) NOT NULL
+       `user_email` VARCHAR(30) REFERENCES user(`email`) NOT NULL,
+       `stock_code` CHAR(6) REFERENCES stock(code) NOT NULL,
+       `stock_market` CHAR(6) REFERENCES stock(market) NOT NULL,
+       `trade_type` CHAR(3) CHECK (`trade_type` IN('ask','bid')) NOT NULL,
+       `price` NUMERIC(15,2) NOT NULL, /*거래체결 가격*/
+       `trade_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+
+/*사용자의 주식 보유정보*/
+DROP TABLE IF EXISTS `user_stock`;
+CREATE TABLE IF NOT EXISTS `user_stock`(
+       `id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+       `user_email` VARCHAR(30) REFERENCES user(`email`) NOT NULL,
+       `stock_code` CHAR(6) REFERENCES stock(code) NOT NULL
 );
